9e0e6cd08f6947b537029af717b88c0e00ef1dbb
Ares-Duong <98302769+Ares-Duong@users.noreply.github.com>
2022-02-25 16:13:37 -0600

    Updated with comments

--- am64main.js
+++ am64main.js
@@ -5,14 +5,20 @@ const token = "8MkGu8tDM4AHKB69seAD_2mkaOxYlO7CR0xVP-UcQIkI2GnWJwiZ1TkLEq9nrHmjb
 const org = 'rmm180000@utdallas.edu'
 const bucket = 'mock_I'
 
+// links to the influx database indicated by the url using the token
 const database = new InfluxDB({
   url: url,
   token: token,
 })
+
+// writeApi allows write and query access to the bucket
 const writeApi = database.getWriteApi(org, bucket)
 writeApi.useDefaultTags({host: 'host1'})
 
-// retrieved from coap
+// Interval between data collection
+const dataPollInt = 1000
+
+// data will be filled out with PollData
 data = {
   "time": new Date(),
   "measurement": "motionReading",
@@ -20,17 +26,47 @@ data = {
   "value": 0
 }
 
+// ipList will be populated through GetIPs
+//let ipList = []
+
 console.log(data)
 
-//SendToDatabase
-try {
-  datapoint = new Point(data["measurement"]).tag("sensor",data["sensor"]).floatField("value",data["value"]).timestamp(new Date())
-  writeApi.writePoint(datapoint)
-  console.log("Sent datapoint to database.")
-} catch (err) {
-  console.log("Error while sending to database. Error: " + err)
+// while on, every dataPollInt
+//   GetIPs
+//   for each ip in iplist except last (border router)
+//     PollData
+       SendToDatabase()
+
+function GetIPs() {
+  // ipList = topology.connectedDevices
 }
 
-//GetIPs =
+// CoAP request
+function PollData() {
+  // The following code is copied from the LED code
+/*
+  export function getCoapLedStatus(targetIP) {
+    getOptions.host = targetIP;
+    const getRequest = coap.request(getOptions);
 
-//PollData =
+    getRequest.on('response', (getResponse) => {
+      ledStates.rled = !!getResponse.payload.readUInt8(0);
+      ledStates.gled = !!getResponse.payload.readUInt8(1);
+    });
+
+    getRequest.end();
+    return ledStates;
+  }
+*/
+}
+
+//send a datapoint to the database
+function SendToDatabase() {
+  try {
+    datapoint = new Point(data["measurement"]).tag("sensor",data["sensor"]).floatField("value",data["value"]).timestamp(new Date())
+    writeApi.writePoint(datapoint)
+    console.log("Sent datapoint to database.")
+  } catch (err) {
+    console.log("Error while sending to database. Error: " + err)
+  }
+}
